fc470e0db8ae69d96e4fee67b3d738a08bbe3827
mazen <spocre714@gmail.com>
2025-05-14 11:41:45 +0300

    Add TestCICD component to verify CI/CD pipeline

---
+++ app/components/TestCICD.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const TestCICD: React.FC = () => {
+  return (
+    <div className="test-cicd">
+      <h2>CI/CD is working!</h2>
+      <p>This component was added to test the GitHub Actions workflow.</p>
+      <p>Last updated: May 14, 2025</p>
+    </div>
+  );
+};
+
+export default TestCICD; 
